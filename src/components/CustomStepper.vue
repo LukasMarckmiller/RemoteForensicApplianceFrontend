915e3556f83e0d0bf3e23df093109107876aa8c3
Lukas Marckmiller <l.marckmiller@hm.edu>
2019-05-24 13:25:38 +0200

    Implemented Summary Bugfixes and Improvements

--- src/components/CustomStepper.vue
+++ src/components/CustomStepper.vue
@@ -7,7 +7,7 @@
              timeout="6000"
              multi-line
              top>
-             If SmartMode is turned on, the application decides itself weather to send the full image of the input device or just specific artifacts to the CERT servers.
+             {{snackbarContent}}
          <v-btn  color="deep-orange lighten-2"
                  flat
                  @click="snackbarInfoSmartMode = false">
@@ -29,7 +29,7 @@
                 <v-stepper-items>
                     <v-stepper-content step="1">
                         <v-layout >
-                            <v-flex xs6 sm6>
+                            <v-flex xs12 sm6>
                         <v-card class="mb-3"
                                 color="grey lighten-4">
                                 <v-layout >
@@ -37,10 +37,10 @@
                                     <v-spacer></v-spacer>
                                     <v-btn flat icon color="deep-orange lighten-2" @click="reloadPage"><v-icon>cached</v-icon></v-btn>
                                 </v-layout>
-                            <v-select chips prepend-icon="sd_storage" class="mr-3 ml-3 red--text" v-model="select" item-value="serial_number" v-bind:items="devices" persistent-hint
+                            <v-select chips prepend-icon="sd_storage" class="mr-3 ml-3 red--text" v-model="selectedInputDevice" item-value="serial_number" v-bind:items="devices" persistent-hint
                                       return-object
                                       single-line label="Select">
-                                <template v-if="select !== ''" slot="selection" slot-scope="data">
+                                <template v-if="selectedInputDevice !== ''" slot="selection" slot-scope="data">
                                     <v-chip>{{ data.item.name}} (Vendor: {{ data.item.vendor}}, Model: {{data.item.model}}, Size: {{parseDeviceCapazityinGB(data.item.size_bytes)}} GB)</v-chip>
                                 </template>
                                 <template v-if="devices !== null" slot="item" slot-scope="data">
@@ -53,7 +53,7 @@
                                 </v-card-title>
                                 <v-list class="grey lighten-4">
                                     <v-list-tile
-                                        v-for="item in select.partitions"
+                                        v-for="item in selectedInputDevice.partitions"
                                         :key="item.name">
                                         <v-icon class="mr-3">storage</v-icon>
                                         <v-list-tile-content>
@@ -63,18 +63,18 @@
                                     </v-list-tile>
                                 </v-list>
                             <b-progress class="mt-2">
-                                <template v-for="(child, index) in select.partitions">
-                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,select.size_bytes)" v-if="index <= 0" variant="primary">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
-                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,select.size_bytes)" v-else-if="index === 1" variant="danger">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
-                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,select.size_bytes)" v-else-if="index === 2" variant="warning">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
-                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,select.size_bytes)" v-else-if="index === 3" variant="info">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
-                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,select.size_bytes)" v-else-if="index >= 4" variant="success">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
+                                <template v-for="(child, index) in selectedInputDevice.partitions">
+                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,selectedInputDevice.size_bytes)" v-if="index <= 0" variant="primary">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
+                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,selectedInputDevice.size_bytes)" v-else-if="index === 1" variant="danger">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
+                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,selectedInputDevice.size_bytes)" v-else-if="index === 2" variant="warning">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
+                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,selectedInputDevice.size_bytes)" v-else-if="index === 3" variant="info">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
+                                    <b-progress-bar :key="child" :value="computePartitionUsedPercentage(child.size_bytes,selectedInputDevice.size_bytes)" v-else-if="index >= 4" variant="success">{{child.name}} {{parseDeviceCapazityinGB(child.size_bytes)}}GB</b-progress-bar>
                                 </template>
                             </b-progress>
                             </v-card>
                         </v-card>
                             </v-flex>
-                            <v-flex xs6 sm6>
+                            <v-flex xs12 sm6>
                                 <v-card class="ml-3"
                                         color="grey lighten-4">
 
@@ -83,7 +83,7 @@
                                         <v-spacer></v-spacer>
                                         <v-btn flat icon color="deep-orange lighten-2" @click="reloadPage"><v-icon>cached</v-icon></v-btn>
                                     </v-layout>
-                                    <v-switch class="ml-3" color="orange darken-3" @change="ToogleSnackBarSmartModeValue"
+                                    <v-switch :disabled="lockSmartMode" class="ml-3" color="orange darken-3" @change="toogleSnackBarSmartModeValue(smartModeInfoText)"
                                               hide-details :label="`SmartMode: ${smartMode == true ? 'On' : 'Off'}`"
                                               v-model="smartMode">
                                     </v-switch>
@@ -95,17 +95,17 @@
                                         </template>
 
                                         <template v-if="devices !== null" slot="item" slot-scope="data">
-                                         {{ data.item.mount_point}} (Size: {{parseDeviceCapazityinGB(data.item.size_bytes)}} GB, Type: {{ data.item.type}}, Read-Only: {{data.item.read_only}})
+                                            {{ data.item.mount_point}} (Size: {{parseDeviceCapazityinGB(data.item.size_bytes)}} GB, Type: {{ data.item.type}}, Read-Only: {{data.item.read_only}})
                                         </template>
                                     </v-select>
-                                    <b-progress v-if="mount !== ''" class="mt-2">
-                                    <b-progress-bar :key="free" :value="computePartitionUsedPercentage(cachedDiskUsage.free, cachedDiskUsage.all)" variant="success">Free {{parseDeviceCapazityinGB(cachedDiskUsage.free)}}GB</b-progress-bar>
-                                    <b-progress-bar :key="used" :value="computePartitionUsedPercentage(cachedDiskUsage.used, cachedDiskUsage.all)" variant="primary">Used {{parseDeviceCapazityinGB(cachedDiskUsage.used)}}GB</b-progress-bar>
-                                </b-progress>
+                                    <b-progress v-if="mount !== '' && !smartMode" class="mt-2">
+                                        <b-progress-bar :key="free" :value="computePartitionUsedPercentage(cachedDiskUsage.free, cachedDiskUsage.all)" variant="success">Free {{parseDeviceCapazityinGB(cachedDiskUsage.free)}}GB</b-progress-bar>
+                                        <b-progress-bar :key="used" :value="computePartitionUsedPercentage(cachedDiskUsage.used, cachedDiskUsage.all)" variant="primary">Used {{parseDeviceCapazityinGB(cachedDiskUsage.used)}}GB</b-progress-bar>
+                                    </b-progress>
                                 </v-card>
                             </v-flex>
                         </v-layout>
-                        <v-btn color="deep-orange lighten-2" v-bind:disabled="select === ''" @click="e1 = 3">
+                        <v-btn color="deep-orange lighten-2"  v-bind:disabled="selectedInputDevice === '' || (smartMode === true && transferModeComputed === false || smartMode === false && mount === '')" @click="e1 = 3">
                             Next
                         </v-btn>
                     </v-stepper-content>
@@ -119,6 +119,24 @@
                         <v-btn flat @click="e1 = 1">Back</v-btn>
                     </v-stepper-content>
                     <v-stepper-content step="3">
+                        <v-expansion-panel value="1" popout >
+                            <v-expansion-panel-content>
+                                <template v-slot:actions>
+                                    <v-icon color="deep-orange lighten-2">expand_more</v-icon>
+                                </template>
+                                <template v-slot:header>
+                                    <div><b>Summary</b></div>
+                                </template>
+                                <v-card class="ml-2">
+                                    <v-card-text v-if="cachedImageOptions !== null">
+                                        <p>Estimated Transmission Time : <i>{{getTimeStringFromSeconds(this.cachedImageOptions.estimated_secs)}}</i> </p>
+                                        <p>Transfer Option : <i>{{this.cachedImageOptions.image_option.type === imageTypeFull ? "Full image" : "Certain Artifacts"}}</i></p>
+                                        <p>Selected Input Device : <i>{{ selectedInputDevice.name}} (Vendor: {{ selectedInputDevice.vendor}}, Model: {{selectedInputDevice.model}}, Size: {{parseDeviceCapazityinGB(selectedInputDevice.size_bytes)}} GB)</i></p>
+                                        <p>Selecte Output: <i>{{cachedImageOptions.image_option.target === imageTargetLocal ?  mount.mount_point + " (Size " + parseDeviceCapazityinGB(mount.size_bytes) + "GB)" : "Remote transmission" }}</i></p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
                         <v-expansion-panel popout>
                             <v-expansion-panel-content class="grey lighten-4">
                                 <template v-slot:actions>
@@ -140,12 +158,20 @@
                             </v-layout>
                             </v-expansion-panel-content>
                         </v-expansion-panel>
-                        <v-btn color="deep-orange lighten-2" @click="postImageJobAndRun(select.name)">Start</v-btn>
+                        <v-btn color="deep-orange lighten-2" @click="postImageJobAndRun(selectedInputDevice.name)">Start</v-btn>
                         <v-btn flat @click="e1 = 2">Back</v-btn>
                     </v-stepper-content>
                 </v-stepper-items>
             </v-stepper>
-            <v-progress-linear class="mt-0" :indeterminate="e1 === 3 && running" :color="e1 ===3 && finisedJob ? 'green' : 'deep-orange lighten-2'" :value="e1 === 3 && finisedJob ? 100 : 0"></v-progress-linear>
+            <v-progress-linear v-if="e1 === 1" class="mt-0" :indeterminate="smartModeProgress" color="deep-orange lighten-2"></v-progress-linear>
+            <v-flex
+                    v-if="e1 === 1 && smartModeProgress"
+                    xs12
+                    subtitle-1
+                    text-xs-center>
+                Calculating best transfer options
+            </v-flex>
+            <v-progress-linear v-if="e1 === 3" class="mt-0" :indeterminate="running" :color="finisedJob ? 'green' : 'deep-orange lighten-2'" :value="finisedJob ? 100 : 0"></v-progress-linear>
         </v-flex>
     </v-container>
      </v-layout>
@@ -153,7 +179,7 @@
 </template>
 
 <script>
-    import axios from "axios"
+    import HTTP from "../HTTPClient";
 
     export default {
       props: {
@@ -162,7 +188,11 @@
       data() {
           return {
               snackbarInfoSmartMode: false,
+              snackbarContent : "",
+              smartModeInfoText : "If SmartMode is turned on, the application decides itself weather to send the full image of the input device or just specific artifacts to the CERT servers.",
+              noRemoteConnectionText : "No connection to the servers possible, please plug in and specify an output device or use the internal storage to continue the image process.",
               smartMode: true,
+              lockSmartMode : false,
               running : false,
               finisedJob :false,
               currentJobId : "",
@@ -172,17 +202,29 @@
               devices : null,
               mounts: null,
               mount: "",
-              e1: 0,
-              select: "",
+              e1: 1,
+              selectedInputDevice: "",
               pollinHandler : null,
+              imageOptions : null,
               cachedDiskUsage : {
                   "all": 0,
                   "used": 0,
                   "free": 0
               },
+              imageTypeFull : 0,
+              imageTypePart : 1,
+              imageTargetRemote: 0,
+              imageTargetLocal:1,
+              smartModeProgress : false,
+              transferModeComputed : false,
+              cachedImageOptions: null,
           }
       },
-
+      watch: {
+          selectedInputDevice(){
+              this.triggerSmartMode();
+          }
+      },
       methods: {
           parseDeviceCapazityinGB: function (bytesAsString) {
               return Math.round(parseInt(bytesAsString) / 1073741824).toString();
@@ -192,31 +234,41 @@
               return (parseInt(bytesAsStringPart) / parseInt(bytesAsStringDevice)) * 100
           },
 
-          reloadPage() {
+          reloadPage: function() {
               window.location.reload()
           },
 
-          postImageJobAndRun: function (path) {
-              this.currentJobId = ""
-              this.consoleInputDeviceOutput = ""
-              this.consoleOutputDeviceOutput = ""
-              this.finisedJob = false
+          getTimeStringFromSeconds: function (seconds){
+              if (seconds === "")
+                  return "";
 
-              axios.post('http://localhost:8000/image', {path: path}).then(response => {this.currentJobId = response.data
-                  this.polling()
+              var date = new Date(null);
+              date.setSeconds(seconds);
+               //take hh:mm:ss
+              return date.toISOString().substr(11, 8)
+          },
+
+          postImageJobAndRun: function (path) {
+              this.currentJobId = "";
+              this.consoleInputDeviceOutput = "";
+              this.consoleOutputDeviceOutput = "";
+              this.finisedJob = false;
+
+               HTTP.post('image', {path: path}).then(response => {this.currentJobId = response.data;
+                  this.polling();
                   this.pollinHandler = setInterval(this.polling, 3000)})
 
           },
 
           polling: function () {
 
-              axios.get('http://localhost:8000/image/' + this.currentJobId).then(response => {
-                  this.pollingResult = response.data
-                  this.consoleOutputDeviceOutput = this.pollingResult.commandOfOutput
-                  this.consoleInputDeviceOutput = this.pollingResult.commandIfOutput
-                  this.running = this.pollingResult.running
+              HTTP.get('image/' + this.currentJobId).then(response => {
+                  this.pollingResult = response.data;
+                  this.consoleOutputDeviceOutput = this.pollingResult.commandOfOutput;
+                  this.consoleInputDeviceOutput = this.pollingResult.commandIfOutput;
+                  this.running = this.pollingResult.running;
                   if (!this.running) {
-                      clearInterval(this.pollinHandler)
+                      clearInterval(this.pollinHandler);
                       this.finisedJob = true
                   }})
           },
@@ -226,32 +278,58 @@
                   return
               }
 
-              axios.get('http://localhost:8000/mounted/' + encodeURIComponent("\"" + mountPoint + "\"")).then(response => {
+              HTTP.get('mounted/' + encodeURIComponent("\"" + mountPoint + "\"")).then(response => {
                   this.cachedDiskUsage = response.data
               })
           },
 
-          ToogleSnackBarSmartModeValue : function (){
-              let val = this.snackbarInfoSmartMode
+          toogleSnackBarSmartModeValue : function (message){
+              let val = this.snackbarInfoSmartMode;
+              this.snackbarContent = message;
+
               if (this.snackbarInfoSmartMode){
-                  this.snackbarInfoSmartMode = false
+                  this.snackbarInfoSmartMode = false;
                   return val
               }
               else{
-                  this.snackbarInfoSmartMode = true
+                  this.snackbarInfoSmartMode = true;
                   return val
+              }
+          },
+
+          triggerSmartMode : function (){
+              this.transferModeComputed = false;
+              this.smartMode = true;
+
+              if (this.selectedInputDevice !== ""){
+                  this.smartModeProgress = true;
+                  HTTP.post('media/transfer',{name:this.selectedInputDevice.name, size:this.selectedInputDevice.size_bytes}).then(response => {
+                        this.cachedImageOptions = response.data;
+                      if (this.cachedImageOptions.image_option.target === this.imageTargetLocal)
+                      {
+                          this.smartMode = false;
+                          this.lockSmartMode = true;
+                          this.toogleSnackBarSmartModeValue(this.noRemoteConnectionText)
+                      }
+                      else
+                          this.smartMode = true;
+
+                      this.smartModeProgress = false;
+                      this.transferModeComputed = true
+                  })
+
               }
           }
       },
 
-      created() {
-         axios.get('http://localhost:8000/media').then(response => {
+      mounted() {
+          HTTP.get('media').then(response => {
              this.devices = response.data
              if (this.devices !== null)
-                 this.select = this.devices[0]
+                 this.selectedInputDevice = this.devices[0]
          })
 
-          axios.get('http://localhost:8000/mounted').then(response => {
+          HTTP.get('mounted').then(response => {
               this.mounts = response.data
           })
       },
